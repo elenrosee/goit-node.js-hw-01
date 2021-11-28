15dc471e707bee9bc643278709ae51d8d62142db
Elena Ovcharenko <elen_rosee@MacBook-Pro-Ovcarenko.local>
2021-11-28 11:28:50 +0200

    done

--- index.js
+++ index.js
@@ -1,4 +1,39 @@
-const date = new Date();
+const argv = require("yargs").argv;
 
-console.log(date.getFullYear());
-console.log("Hello Node.js");
+const contactsOperations = require("./contactsOperations");
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const contacts = await contactsOperations.listContacts();
+      console.table(contacts);
+      break;
+
+    case "get":
+      const contact = await contactsOperations.getContactById(id);
+      if (!contact) {
+        throw new Error(`Contact with id=${id} not found`);
+      }
+      console.log(contact);
+      break;
+
+    case "add":
+      const newContact = await contactsOperations.addContact({
+        name,
+        email,
+        phone,
+      });
+      console.log(newContact);
+      break;
+
+    case "remove":
+      const removeContact = await contactsOperations.removeContactById(id);
+      console.log(removeContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+invokeAction(argv);
